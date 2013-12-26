a065292f5949629ada4393285f4e33ae93f277a7
rcl2748 <rcl2748@ya.ru>
2013-12-25 23:12:16 -0600

    Update!

--- src/main/java/net/PixelizedMC/BossMessage/Commands.java
+++ src/main/java/net/PixelizedMC/BossMessage/Commands.java
@@ -1,8 +1,11 @@
 package net.PixelizedMC.BossMessage;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.lang.NumberUtils;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -28,25 +31,46 @@ public class Commands {
     					sender.sendMessage(CM.noperm);
     					return true;
     				}
-    				if (args.length != 3)
+    				if (args.length > 2) {
+    					if (NumberUtils.isNumber(args[1])) {
+    						List<String> arraymsg = Arrays.asList(args);
+    						arraymsg.remove(0);
+    						arraymsg.remove(0);
+    						String textmsg = StringUtils.join(arraymsg, " ");
+    						
+    						List<String> message = new ArrayList<>();
+    						message.add(textmsg);
+    						message.add(args[1]);
+    						
+    						CM.messages.add(message);
+    						CM.rawmessages.add(message);
+    						CM.save();
+    						sender.sendMessage(ChatColor.GREEN + "Your message was successfully added!");
+	    				}
+    				} else {
+    					sender.sendMessage(ChatColor.DARK_RED + "Usage: " + ChatColor.RED + "/bm add <percent> <message>");
+    				}
     				
-    				sender.sendMessage("nyi");
     			} else if (args[0].equalsIgnoreCase("remove")) {
     				
     				if (!sender.hasPermission("bossmessage.remove")) {
     					sender.sendMessage(CM.noperm);
     					return true;
     				}
-    				
-    				if (NumberUtils.isNumber(args[1])) {
-    					int num = Integer.parseInt(args[1]);
-    					if (CM.messages.size() >= num && num > 0) {
-    						CM.messages.remove(num - 1);
-    						CM.rawmessages.remove(num - 1);
-    						CM.config.set("BossMessage.Messages", CM.rawmessages);
-    						CM.save();
-    						Lib.resetCount();
-    					}
+    				if (args.length == 2) {
+	    				if (NumberUtils.isNumber(args[1])) {
+	    					int num = Integer.parseInt(args[1]);
+	    					if (CM.messages.size() >= num && num > 0) {
+	    						CM.messages.remove(num - 1);
+	    						CM.rawmessages.remove(num - 1);
+	    						CM.config.set("BossMessage.Messages", CM.rawmessages);
+	    						CM.save();
+	    						Lib.resetCount();
+	    						sender.sendMessage(ChatColor.GREEN + "Message #" + num + " was successfully removed!");
+	    					}
+	    				}
+    				} else {
+    					sender.sendMessage(ChatColor.DARK_RED + "Usage: " + ChatColor.RED + "/bm remove <#>");
     				}
     				
     				
