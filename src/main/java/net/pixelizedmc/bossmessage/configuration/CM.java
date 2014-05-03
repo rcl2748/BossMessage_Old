4ea22676b92a7812a9ff1356e6cba2257f63ddab
rcl2748 <rcl2748@ya.ru>
2014-05-03 17:57:07 -0500

    gfg

--- src/main/java/net/pixelizedmc/bossmessage/configuration/CM.java
+++ src/main/java/net/pixelizedmc/bossmessage/configuration/CM.java
@@ -7,7 +7,6 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
